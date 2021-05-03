212be0c3adf12f0a878d2de714a9457b409e4b0e
Jalen <jbethea117@rams.wssu.edu>
2021-05-03 12:20:05 -0400

    Login-in is up now including  pt2

--- app/src/main/java/com/example/mypageapplication/AccountSettings.java
+++ app/src/main/java/com/example/mypageapplication/AccountSettings.java
@@ -1,14 +1,35 @@
 package com.example.mypageapplication;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class AccountSettings extends AppCompatActivity {
+    EditText username, email,bio;
+    Toolbar toolbar;
+    CircleImageView profile;
+    ImageView pick_image;
+    Button update;
+    FirebaseAuth auth;
+    FirebaseUser user;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_settings);
+    }
+    private void init(){
+
     }
 }
